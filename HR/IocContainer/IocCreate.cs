969cdd0ec7f53c6737623b20d640570396bf88db
MFSNXY <1911923295@qq.com>
2019-09-26 09:43:52 +0800

    HR

---
+++ HR/IocContainer/IocCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace IocContainer
+{
+    public class IocCreate
+    {
+
+        public static TIDAO CreateDao<TIDAO, TDAO>() where TDAO : TIDAO
+        {
+            UnityContainer ioc = new UnityContainer();
+            ioc.RegisterType<TIDAO, TDAO>();
+            return ioc.Resolve<TIDAO>();
+        }
+
+        public static TIBLL CreateBLL<TIBLL>(string name)
+        {
+            UnityContainer ioc = new UnityContainer();
+            ExeConfigurationFileMap ef = new ExeConfigurationFileMap();
+            ef.ExeConfigFilename = "E:/TDXY/新建文件夹/HR/HR/Unity.config";
+            Configuration cf = ConfigurationManager.OpenMappedExeConfiguration(ef, ConfigurationUserLevel.None);
+            UnityConfigurationSection cs = cf.GetSection("unity") as UnityConfigurationSection;
+            ioc.LoadConfiguration(cs, "containerTwo");
+            return ioc.Resolve<TIBLL>(name);
+        }
+
+    }
+}
