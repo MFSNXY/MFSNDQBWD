6eabf21c2f49999389f56a6c5bebf8afac900bc3
MFSNXY <1911923295@qq.com>
2019-10-17 17:39:08 +0800

    不重要

---
+++ HR/EFEntity/ConfigMajor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFEntity
+{
+    public class ConfigMajor
+    {
+        public int Id { get; set; }
+        public string Majorkindid { get; set; }
+        public string Majorkindname { get; set; }
+        public string Majorid { get; set; }
+        public string Majorname { get; set; }
+        public int Testamount { get; set; }
+    }
+}
